9c15cd3bbf4b4dbec9ae67872f1af70b88bc1106
JakeM <quangmaiduy1993@gmail.com>
2020-12-01 20:02:55 +1300

    DONE

--- showdan.py
+++ showdan.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
 """
-
-A Shodan/ZoomEye scan for IP range
+A Shodan/ZoomEye scan for IP range (potentially reverse DNS lookup)
 Needs to install shodan through pip: python3 -m pip3 install shodan
 Needs to install zoomeye through pip: python3 -m pip3 install zoomeye-sdk
-
 """
 
 import shodan
@@ -15,76 +13,70 @@ import time
 import ipaddress as ipadd
 
 
-SHODAN_API_KEY='' # Shodan api key here
-ZOOMEYE_API_KEY='' # Zoomeye api key here
-shodan_api=shodan.Shodan(SHODAN_API_KEY)
-zoomeye_api=zoomeye.ZoomEye(api_key=ZOOMEYE_API_KEY)
+SHODAN_API_KEY = ''  # Shodan api key here
+ZOOMEYE_API_KEY = ''  # Zoomeye api key here
+shodan_api = shodan.Shodan(SHODAN_API_KEY)
+zoomeye_api = zoomeye.ZoomEye(api_key=ZOOMEYE_API_KEY)
 
 
+# Check usage
 def check():
-    if len(sys.argv)==1:
-        print("Must enter ip/or ip range")
+    if len(sys.argv) != 2:
+        print("Invalid argument!")
+        print("Usage: " + sys.argv[0] + " \"valid IPv4 address or IPv4 Range in CIDR Notation\"")
         sys.exit()
+
 
 def scan():
     # Take arg and convert CIDR to IP addresses
-    ipRange=sys.argv[1]
+    ipRange = sys.argv[1]
     try:
         for eachIP in ipadd.IPv4Network(ipRange):
-            print("Scanning: "+str(eachIP))
-
+            print("\nSCANNING: "+str(eachIP))
+            print("*"*22+" SHODAN RESULT "+"*"*22)
             # Pulling Shodan API data
             try:
-                host=shodan_api.host(str(eachIP))
-                print("*"*30+" SHODAN RESULT "+"*"*30)
-                print("*"*60)
+                host = shodan_api.host(str(eachIP))
 
-                # More fields can  be add here if more information is desired
+                # More fields can be added here if more information is desired
                 print("""IP: {} \nOpen Ports: {} \nOrganization: {} \nOperating System: {}\n"""
-                        .format(host['ip_str'],host['ports'], host.get('org', 'n/a'), host.get('os', 'n/a')))
+                        .format(host['ip_str'], host['ports'], host.get('org', 'n/a'), host.get('os', 'n/a')))
 
                 for item in host['data']:
                     print("""Port: {} \nBanner: {}""".format(item['port'], item['data']))
                     try:
                         print("Organisation: {}".format(item['ssl']['cert']['subject']['O']))
-                        print("*"*60)
-                    except:
+                    except Exception:
                         pass
                 time.sleep(1)
-            except shodan.APIError:
+            except shodan.APIError as e:
+                print("{}".format(e))
                 time.sleep(1)
                 pass
 
-            # Pulling ZoomEye API data 
-            try:
-                data=zoomeye_api.dork_search(eachIP)
-                print("*"*30+" ZOOMEYE RESULT "+"*"*30)
-                print("*"*60)
+            # Pulling ZoomEye API data
+            data = zoomeye_api.dork_search(eachIP)
+            print("*"*22+" ZOOMEYE RESULT "+"*"*22)
 
-                for item in data:
-                # TO DO:
+            for item in data:
+                if (len(str(item['portinfo'])) > 200):
+                    print("""Port: {}\nHostname: {}\nVersion: {}\nService: {}\nDevice: {}\nExtra Info: {}\nOS: {}\nApp Title: {}\nBanner: {}""".format(item['portinfo']['port'], item['portinfo']['hostname'], item['portinfo']['version'], item['portinfo']['service'], item['portinfo']['device'], item['portinfo']['extrainfo'], item['portinfo']['os'], item['portinfo']['app'], item['portinfo']['banner']))
+                else:
+                    print("No information available for that IP.")
 
-
-            except:
-                time.sleep(1)
-                pass
-
+    # Catch error when provided IP is invalid
     except ipadd.AddressValueError as e:
         print("Invalid IP: {}".format(e))
         sys.exit()
 
-    print("Shodan scan completed!")
+    print("\nSCAN COMPLETED!   ¯\_(ツ)_/¯   ")
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     check()
     scan()
 
-
 '''
 To be implemented:
-ipinfo.io? ip info
-greynoise? ip info
-SecurityTrails? DNS reverse lookup
-ZoomEye? host info
+SecurityTrails? DNS reverse lookup mode?
 '''
-
